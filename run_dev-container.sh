db4507deda91e825c2df9d5798dc62ff62f98371
mu-777 <water.tree.rainbow@gmail.com>
2022-02-26 14:56:42 +0900

    Support GUI apps

--- run_dev-container.sh
+++ run_dev-container.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-docker run --rm -itd \
+if [ -e $DISPLAY ]; then
+  echo "Not found DISPLAY envvar"
+else
+  echo "You should run x11 server(with no RANDR) for GUI applications(rviz, rqt)"
+fi
+
+docker run --rm -itdz \
   -v /etc/group:/etc/group:ro \
   -v /etc/passwd:/etc/passwd:ro \
   -v /etc/shadow:/etc/shadow:ro \
@@ -8,5 +14,7 @@ docker run --rm -itd \
   -u $(id -u $USER):$(id -g $USER) \
   -v ${PWD}:${PWD} \
   -w ${PWD} \
+  -e DISPLAY=${DISPLAY} \
+  -v /tmp/.X11-unix:/tmp/.X11-unix \
   --name ros2-study-dev \
   mu777/ros2:foxy_dev /bin/sh -c "while : ; do sleep 3; done"
